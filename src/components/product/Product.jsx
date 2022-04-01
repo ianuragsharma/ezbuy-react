ba37a6049297e891117c8d2a381dc71c1280662d
Anurag Sharma <anuragsharma0711@gmail.com>
2022-04-01 22:22:25 +0530

    refactor and minor changes

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,22 +1,22 @@
 import "./product.css";
 const Product = ({ product }) => {
-  const productDiscount = (price) => {
-    const discount = product.price - (product.discount * product.price) / 100;
-    return discount;
-  };
-
   return (
     <>
       <div className="vertical-card flex-column card-dismiss ">
-        <span className="text-center">
+        <span className="text-xs text-center text-black rating">
+          {product.rating}
+          <i className="fa-solid fa-star text-primary"></i> |{" "}
+          {product.allRating}
+        </span>
+        <span className="text-center wishlist">
           <i className="fa-solid fa-heart"></i>
         </span>
         <img src={product.imgURL} loading="lazy" alt={product.imgAlt} />
         <div className="card-details-container">
-          <h3 className="text-base">{product.title}</h3>
+          <h3 className="text-base fw-500">{product.title}</h3>
           <span className="text-xs">{product.description}</span>
           <p className="card-price-info">
-            Rs.{productDiscount()}
+            Rs.{product.discountPrice}
             <del className="text-xs"> Rs.{product.price}</del>
           </p>
         </div>
