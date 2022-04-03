e2103cb9e9193cea83970b4dba57b30ce849239f
Anurag Sharma <anuragsharma0711@gmail.com>
2022-04-03 23:46:21 +0530

    feat:Added a route to products if cart is empty

--- src/screens/CartScreen/CartScreen.jsx
+++ src/screens/CartScreen/CartScreen.jsx
@@ -1,16 +1,28 @@
 import "./cartScreen.css";
 import { CartProduct, CartPrice } from "../../components";
 import { useCart } from "../../contexts";
+import { Link } from "react-router-dom";
 const CartScreen = () => {
   const { cartState } = useCart();
   return (
     <div>
-      <h3 className="text-center cart-heading">Cart({cartState.length})</h3>
+      <h3 className="text-center cart-heading fw-500">
+        My Cart({cartState.length})
+      </h3>
       <section className="cart-container flex-row">
         <div className="cart-products flex-row">
-          <CartProduct />
+          {cartState.length > 0 ? (
+            <CartProduct />
+          ) : (
+            <h4 className="fw-300">
+              Oh! you dont have any products in your cart{" "}
+              <Link className="text-primary" to="/products">
+                Lets go Shopping
+              </Link>
+            </h4>
+          )}
         </div>
-        <CartPrice />
+        {cartState.length > 0 && <CartPrice />}
       </section>
     </div>
   );
