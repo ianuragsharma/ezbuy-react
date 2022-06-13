02b269591b354aea08fbe79fef0df1f35c172343
Anurag Sharma <anuragsharma0711@gmail.com>
2022-06-13 12:57:36 +0530

    style: price card style change

--- src/components/cartPrice/CartPrice.jsx
+++ src/components/cartPrice/CartPrice.jsx
@@ -13,33 +13,31 @@ const CartPrice = () => {
     0
   );
   return (
-    <>
-      <div className="price-container">
-        <h4 className="fw-500 ">Price Details</h4>
-        <div className="price-breakdown">
-          <div className="flex-row price-details">
-            <span>Price ({cartState.length} items)</span>
-            <span>Rs {totalCartPrice}</span>
-          </div>
-          <div className="flex-row price-details">
-            <span>Discount ({cartState.length} items)</span>
-            <span>Rs {cartDiscount}</span>
-          </div>
-          <div className="flex-row price-details">
-            <span>Delivery Charges</span>
-            <span className="text-primary">Free</span>
-          </div>
-          <div className="flex-row price-details">
-            <span className="fw-500">Total Amount</span>
-            <span className="fw-500">Rs {totalCartPrice - cartDiscount}</span>
-          </div>
+    <div className="price-container my-1">
+      <h4 className="fw-500 text-xl price-details">Price Details</h4>
+      <div className="price-breakdown">
+        <div className="flex-row price-details">
+          <span>Price ({cartState.length} items)</span>
+          <span>Rs {totalCartPrice}</span>
         </div>
-        <p className="fw-500">You will save Rs.{cartDiscount} on this order</p>
-        <button className="btn btn-solid-primary order-btn text-white">
-          Place Order
-        </button>
+        <div className="flex-row price-details">
+          <span>Discount ({cartState.length} items)</span>
+          <span>Rs {cartDiscount}</span>
+        </div>
+        <div className="flex-row price-details">
+          <span>Delivery Charges</span>
+          <span className="text-primary">Free</span>
+        </div>
+        <div className="flex-row price-details">
+          <span className="fw-500">Total Amount</span>
+          <span className="fw-500">Rs {totalCartPrice - cartDiscount}</span>
+        </div>
       </div>
-    </>
+      <p className="fw-500">You will save Rs.{cartDiscount} on this order</p>
+      <button className="btn btn-solid-primary order-btn text-white  text-base">
+        Place Order
+      </button>
+    </div>
   );
 };
 
