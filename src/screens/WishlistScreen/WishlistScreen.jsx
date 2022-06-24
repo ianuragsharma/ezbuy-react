f4f029a716504521c62466a014705724d68b5240
Anurag Sharma <anuragsharma0711@gmail.com>
2022-06-24 11:17:18 +0530

    style: minor changes in style

--- src/screens/WishlistScreen/WishlistScreen.jsx
+++ src/screens/WishlistScreen/WishlistScreen.jsx
@@ -10,23 +10,21 @@ const WishlistScreen = () => {
     <Product key={item._id} product={item} />
   ));
   return (
-    <>
-      <section className="wishlist-container">
-        <h3 className="fw-500 wishlist-heading text-center">My Wishlist</h3>
-        <div className="wishlist-product flex-row">
-          {wishlistState.length > 0 ? (
-            wishlistItems
-          ) : (
-            <h4 className="fw-300">
-              Oh! you dont have any products in your Wishlist{" "}
-              <Link className="text-primary" to="/products">
-                Lets go Shopping
-              </Link>
-            </h4>
-          )}
-        </div>
-      </section>
-    </>
+    <section className="wishlist-container">
+      <h4 className="fw-500 wishlist-heading text-center">My Wishlist</h4>
+      <div className="wishlist-product flex-row">
+        {wishlistState.length > 0 ? (
+          wishlistItems
+        ) : (
+          <h4 className="fw-300">
+            Oh! you dont have any products in your Wishlist{" "}
+            <Link className="text-primary" to="/products">
+              Lets go Shopping
+            </Link>
+          </h4>
+        )}
+      </div>
+    </section>
   );
 };
 
